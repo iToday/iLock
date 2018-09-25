ef95468f84fb7909afad7d3a30bfb9cd66514b06
iToday <xba1987@126.com>
2018-09-25 15:39:01 +0800

    add framework

---
+++ main.c
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.c
+ * Author: Yijun
+ *
+ * Created on 2018年9月25日, 下午2:04
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
